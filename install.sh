e962593573448cabad1b0c1aae92acc4780763aa
gateway <hugog.valin@gmail.com>
2019-09-24 16:15:31 -0300

    Instalacao da aplicacao como um servico

--- install.sh
+++ install.sh
@@ -1,17 +1,42 @@
 # Instalando dependencias
-sudo apt update
-sudo apt-get -y install mosquitto
-sudo apt-get -y install openvpn
-sudo apt-get -y install python3-pip
-sudo apt-get -y install sqlite3
-pip3 install paho-mqtt
+# sudo apt update
+# sudo apt-get -y install mosquitto
+# sudo apt-get -y install openvpn
+# sudo apt-get -y install python3-pip
+# sudo apt-get -y install sqlite3
+# pip3 install paho-mqtt 
+
 
 #Configurando arquivo de inicializacao automatica
-echo "sudo openvpn --config $(pwd)/conf/client.ovpn --daemon openvpn-gateway" >> init.sh
-echo "nohup python3 $(pwd)/ApiGatewayApplication.py &" >> init.sh
-sudo cp -ra init.sh /etc/init.d/
-sudo chmod 777 /etc/init.d/init.sh
-sudo chown root:root /etc/init.d/init.sh
+echo "#!/bin/bash" > gateway.sh
+echo "service mosquitto start" >> gateway.sh
+echo "openvpn --config /usr/bin/api-gateway/conf/client.ovpn --daemon openvpn-gateway" >> gateway.sh
+echo "nohup /usr/bin/python3 /usr/bin/api-gateway/ApiGatewayApplication.py 2>/dev/null &" >> gateway.sh
+sudo chmod +x gateway.sh
+
+# Criando o servico
+echo "[Unit]" > gateway.service
+echo "Description=Gateway service" >> gateway.service
+echo "" >> gateway.service
+echo "[Service]" >> gateway.service
+echo "Type=simple" >> gateway.service
+echo "ExecStart=/usr/bin/python3 /usr/bin/api-gateway/ApiGatewayApplication.py" >> gateway.service
+echo "" >> gateway.service
+echo "[Install]" >> gateway.service
+echo "WantedBy=multi-user.target" >> gateway.service
+sudo cp -ra gateway.service /etc/systemd/system/gateway.service
+sudo chmod 644 /etc/systemd/system/gateway.service
+sudo systemctl enable gateway
 
 # Permissao para criar nohup da aplicacao
 sudo chmod +x ApiGatewayApplication.py
+
+#Copiando arquivos para /usr/bin/
+sudo rm -rf /usr/bin/api-gateway
+sudo mkdir /usr/bin/api-gateway
+sudo cp -ra * /usr/bin/api-gateway
+sudo chown -R root:root /usr/bin/api-gateway
+
+# Apagando arquivos temporarios
+rm -rf gateway.service
+rm -rf gateway.sh
--- model/dao/ConfigurationDAO.py
+++ model/dao/ConfigurationDAO.py
@@ -1,16 +1,17 @@
 import os
 import configparser
+from os.path import dirname, realpath
 
-os.path.abspath(os.path.join('conf/conf', os.pardir))
 class ConfigurationDAO(object):
     def __init__(self, section):
         self.__section = section
         self.__properties = {}
-        self.__FILE = os.path.abspath(os.path.join('conf/conf', os.pardir)) + '/apigateway.conf'
+        self.__FILE = str(dirname(dirname(dirname(realpath(__file__))))) + '/conf/apigateway.conf'
 
     def get(self, key):
         if ( key not in self.__properties ):
             config = configparser.RawConfigParser()
+            print(self.__FILE)
             config.read(self.__FILE)
             self.__properties[key] =  config.get(self.__section, key)
         return self.__properties[key]
