fba6297b699504e35bd19ba1f65eb36bd2ae4e7f
gateway <hugog.valin@gmail.com>
2019-09-25 14:10:43 -0300

    OpenVPN Service

--- install.sh
+++ install.sh
@@ -1,42 +1,51 @@
-# Instalando dependencias
-# sudo apt update
-# sudo apt-get -y install mosquitto
-# sudo apt-get -y install openvpn
-# sudo apt-get -y install python3-pip
-# sudo apt-get -y install sqlite3
-# pip3 install paho-mqtt 
+installDependencies(){
+    sudo apt update
+    sudo apt-get -y install mosquitto
+    sudo apt-get -y install openvpn
+    sudo apt-get -y install python3-pip
+    sudo apt-get -y install sqlite3
+    sudo -H pip3 install paho-mqtt
+}
 
+installApplication(){
+    sudo rm -rf /usr/bin/api-gateway
+    sudo mkdir /usr/bin/api-gateway
+    sudo cp -ra * /usr/bin/api-gateway
+    sudo chown -R root:root /usr/bin/api-gateway
+}
 
-#Configurando arquivo de inicializacao automatica
-echo "#!/bin/bash" > gateway.sh
-echo "service mosquitto start" >> gateway.sh
-echo "openvpn --config /usr/bin/api-gateway/conf/client.ovpn --daemon openvpn-gateway" >> gateway.sh
-echo "nohup /usr/bin/python3 /usr/bin/api-gateway/ApiGatewayApplication.py 2>/dev/null &" >> gateway.sh
-sudo chmod +x gateway.sh
+createOpenVPNService(){
+    sudo mv client.opvn /etc/openvpn/client.conf
+    sudo systemctl enable openvpn@client.service
+    sudo service openvpn@client start
+}
 
-# Criando o servico
-echo "[Unit]" > gateway.service
-echo "Description=Gateway service" >> gateway.service
-echo "" >> gateway.service
-echo "[Service]" >> gateway.service
-echo "Type=simple" >> gateway.service
-echo "ExecStart=/usr/bin/python3 /usr/bin/api-gateway/ApiGatewayApplication.py" >> gateway.service
-echo "" >> gateway.service
-echo "[Install]" >> gateway.service
-echo "WantedBy=multi-user.target" >> gateway.service
-sudo cp -ra gateway.service /etc/systemd/system/gateway.service
-sudo chmod 644 /etc/systemd/system/gateway.service
-sudo systemctl enable gateway
+createGatewayService(){
+    echo "[Unit]" > gateway.service
+    echo "Description=Gateway Service" >> gateway.service
+    echo "" >> gateway.service
+    echo "[Service]" >> gateway.service
+    echo "Type=simple" >> gateway.service
+    echo "ExecStart=/usr/bin/python3 /usr/bin/api-gateway/ApiGatewayApplication.py" >> gateway.service
+    echo "" >> gateway.service
+    echo "[Install]" >> gateway.service
+    echo "WantedBy=multi-user.target" >> gateway.service
+    sudo cp -ra gateway.service /etc/systemd/system/gateway.service
+    sudo chmod 644 /etc/systemd/system/gateway.service
+    sudo systemctl enable gateway
+    sudo systemctl start gateway
+}
 
-# Permissao para criar nohup da aplicacao
-sudo chmod +x ApiGatewayApplication.py
+deleteTemporaryFiles(){
+    rm -rf gateway.service
+}
 
-#Copiando arquivos para /usr/bin/
-sudo rm -rf /usr/bin/api-gateway
-sudo mkdir /usr/bin/api-gateway
-sudo cp -ra * /usr/bin/api-gateway
-sudo chown -R root:root /usr/bin/api-gateway
+main(){
+    installDependencies
+    installApplication
+    createOpenVPNService
+    createGatewayService
+    deleteTemporaryFiles
+}
 
-# Apagando arquivos temporarios
-rm -rf gateway.service
-rm -rf gateway.sh
+main
